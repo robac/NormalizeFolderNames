7a006d8043923ac92496841c184089052044f355
robac <poch@aristia.cz>
2022-07-27 13:40:47 +0200

    spacing change

--- RenameRecursivelly/Utils/ItemInfo.cs
+++ RenameRecursivelly/Utils/ItemInfo.cs
@@ -1,28 +1,33 @@
 ﻿namespace RenameRecursivelly.Utils
 {
     public class ItemInfo
-{
-    public string path;
-    public string name;
-    public string normalizedName;
-    public bool isDir = false;
-
-    public string Path { get => path; }
-    public string Name { get => name; }
-    public string NormalizedName { get => normalizedName; }
-    public bool IsDir { get => isDir; }
-
-    public string Time { get {
-                return DateTime.Now.ToString();
-            } }
-
-
-    public ItemInfo(string path, string name, string normalizedName, bool isDir)
     {
-        this.name = name;
-        this.normalizedName = normalizedName;
-        this.isDir = isDir;
-        this.path = path;
+        public string path;
+        public string name;
+        public string normalizedName;
+        public bool isDir = false;
+
+        //CSV columns
+        public string Path { get => path; }
+        public string Name { get => name; }
+        public string NormalizedName { get => normalizedName; }
+        public bool IsDir { get => isDir; }
+
+        public string Time 
+        { 
+                get 
+                {
+                    return DateTime.Now.ToString();
+                }      
+        }
+
+
+        public ItemInfo(string path, string name, string normalizedName, bool isDir)
+        {
+            this.name = name;
+            this.normalizedName = normalizedName;
+            this.isDir = isDir;
+            this.path = path;
+        }
     }
-}
 }
