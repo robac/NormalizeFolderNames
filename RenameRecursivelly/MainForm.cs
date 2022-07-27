0d6e7d95e6eeaeb9cc823818afac1653a7cbe50f
robac <poch@aristia.cz>
2022-07-27 13:01:48 +0200

    MainForm rename.

--- RenameRecursivelly/MainForm.cs
+++ RenameRecursivelly/MainForm.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace RenameRecursivelly
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
